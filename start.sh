d922fa43e432cc88ed742f796cf0303576fe19c7
juatho <jamjuhochoi@gmail.com>
2019-12-16 03:44:03 +0000

    pages

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-mkdir -p /var/log;
-chmod -R 777 /var/log;
-mongod --fork --logpath=/var/log/mongodb.log;
-python3 data_acquire.py & python3 app.py;
-#python3 -c 'import pymongo;list(pymongo.MongoClient().get_database("energy").energy.find())';
