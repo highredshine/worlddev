b42f87eb3c36179a75fabd80d506f595d40f93aa
juatho <jamjuhochoi@gmail.com>
2019-12-16 03:39:49 +0000

    website setup

--- app.py
+++ app.py
@@ -1,192 +1,46 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import numpy as np
-import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 
-from database import fetch_all_bpa_as_df
+from pages.main import main_page
+from pages.about import about_page
+from pages.details import details_page
 
-# Definitions of constants. This projects uses extra CSS stylesheet at `./assets/style.css`
-COLORS = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)']
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', '/assets/style.css']
-
-# Define the dash app first
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+app.config.suppress_callback_exceptions = True
 
-# Define component functions
+app.layout = html.Div([
+        dcc.Location(id='url', refresh=False),
+        html.Div(id='page-content')
+    ])
 
-
-def page_header():
-    """
-    Returns the page header as a dash `html.Div`
-    """
-    return html.Div(id='header', children=[
-        html.Div([html.H3('Website Title')],
-                 className="ten columns"),
-        html.A([html.Img(id='logo', src=app.get_asset_url('github.png'),
-                         style={'height': '35px', 'paddingTop': '7%'}),
-                html.Span('Juatho', style={'fontSize': '2rem', 'height': '35px', 'bottom': 0,
-                                                'paddingLeft': '4px', 'color': '#a3a7b0',
-                                                'textDecoration': 'none'})],
-               className="two columns row",
-               href='https://github.com/juatho/'),
-    ], className="row")
+# @app.callback(Output('page-1-content', 'children'),
+#               [Input('page-1-dropdown', 'value')])
+# def page_1_dropdown(value):
+#     return 'You have selected "{}"'.format(value)
 
 
-def description():
-    """
-    Returns overall project description in markdown
-    """
-    return html.Div(children=[dcc.Markdown('''
-        # Project Description
-        ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
+# @app.callback(Output('page-2-content', 'children'),
+#               [Input('page-2-radios', 'value')])
+# def page_2_radios(value):
+#     return 'You have selected "{}"'.format(value)
 
 
-def static_stacked_trend_graph(stack=False):
-    """
-    Returns scatter line plot of all power sources and power load.
-    If `stack` is `True`, the 4 power sources are stacked together to show the overall power
-    production.
-    """
-    df = fetch_all_bpa_as_df()
-    if df is None:
-        return go.Figure()
-    sources = ['Wind', 'Hydro', 'Fossil/Biomass', 'Nuclear']
-    x = df['Datetime']
-    fig = go.Figure()
-    for i, s in enumerate(sources):
-        fig.add_trace(go.Scatter(x=x, y=df[s], mode='lines', name=s,
-                                 line={'width': 2, 'color': COLORS[i]},
-                                 stackgroup='stack' if stack else None))
-    fig.add_trace(go.Scatter(x=x, y=df['Load'], mode='lines', name='Load',
-                             line={'width': 2, 'color': 'orange'}))
-    title = 'Energy Production & Consumption under BPA Balancing Authority'
-    if stack:
-        title += ' [Stacked]'
-    fig.update_layout(template='plotly_dark',
-                      title=title,
-                      plot_bgcolor='#23272c',
-                      paper_bgcolor='#23272c',
-                      yaxis_title='MW',
-                      xaxis_title='Date/Time')
-    return fig
-
-
-def what_if_description():
-    """
-    Returns description of "What-If" - the interactive component
-    """
-    return html.Div(children=[
-        dcc.Markdown('''
-        # Section 1
-        ''', className='eleven columns', style={'paddingLeft': '5%'})
-    ], className="row")
-
-
-def what_if_tool():
-    """
-    Returns the What-If tool as a dash `html.Div`. The view is a 8:3 division between
-    demand-supply plot and rescale sliders.
-    """
-    return html.Div(children=[
-        html.Div(children=[dcc.Graph(id='what-if-figure')], className='nine columns'),
-
-        html.Div(children=[
-            html.H5("Rescale Power Supply", style={'marginTop': '2rem'}),
-            html.Div(children=[
-                dcc.Slider(id='wind-scale-slider', min=0, max=4, step=0.1, value=2.5, className='row',
-                           marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '5rem'}),
-
-            html.Div(id='wind-scale-text', style={'marginTop': '1rem'}),
-
-            html.Div(children=[
-                dcc.Slider(id='hydro-scale-slider', min=0, max=4, step=0.1, value=0,
-                           className='row', marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '3rem'}),
-            html.Div(id='hydro-scale-text', style={'marginTop': '1rem'}),
-        ], className='three columns', style={'marginLeft': 5, 'marginTop': '10%'}),
-    ], className='row eleven columns')
-
-
-def architecture_summary():
-    """
-    Returns the text and image of architecture summary of the project.
-    """
-    return html.Div(children=[
-        dcc.Markdown('''
-            # Section 2
-        ''', className='row eleven columns', style={'paddingLeft': '5%'}),
-
-        html.Div(children=[
-            html.Img(src="https://docs.google.com/drawings/d/e/2PACX-1vQNerIIsLZU2zMdRhIl3ZZkDMIt7jhE_fjZ6ZxhnJ9bKe1emPcjI92lT5L7aZRYVhJgPZ7EURN0AqRh/pub?w=670&amp;h=457",
-                     className='row'),
-        ], className='row', style={'textAlign': 'center'}),
-
-        dcc.Markdown('''
-        
-        ''')
-    ], className='row')
-
-
-# Sequentially add page components to the app's layout
-def dynamic_layout():
-    return html.Div([
-        page_header(),
-        html.Hr(),
-        description(),
-        # dcc.Graph(id='trend-graph', figure=static_stacked_trend_graph(stack=False)),
-        dcc.Graph(id='stacked-trend-graph', figure=static_stacked_trend_graph(stack=True)),
-        what_if_description(),
-        what_if_tool(),
-        architecture_summary(),
-    ], className='row', id='content')
-
-
-# set layout to a function which updates upon reloading
-app.layout = dynamic_layout
-
-
-# Defines the dependencies of interactive components
-
-@app.callback(
-    dash.dependencies.Output('wind-scale-text', 'children'),
-    [dash.dependencies.Input('wind-scale-slider', 'value')])
-def update_wind_sacle_text(value):
-    """Changes the display text of the wind slider"""
-    return "Wind Power Scale {:.2f}x".format(value)
-
-
-@app.callback(
-    dash.dependencies.Output('hydro-scale-text', 'children'),
-    [dash.dependencies.Input('hydro-scale-slider', 'value')])
-def update_hydro_sacle_text(value):
-    """Changes the display text of the hydro slider"""
-    return "Hydro Power Scale {:.2f}x".format(value)
-
-
-
-@app.callback(
-    dash.dependencies.Output('what-if-figure', 'figure'),
-    [dash.dependencies.Input('wind-scale-slider', 'value'),
-     dash.dependencies.Input('hydro-scale-slider', 'value')])
-def what_if_handler(wind, hydro):
-    """Changes the display graph of supply-demand"""
-    df = fetch_all_bpa_as_df(allow_cached=True)
-    x = df['Datetime']
-    supply = df['Wind'] * wind + df['Hydro'] * hydro + df['Fossil/Biomass'] + df['Nuclear']
-    load = df['Load']
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=supply, mode='none', name='supply', line={'width': 2, 'color': 'pink'},
-                  fill='tozeroy'))
-    fig.add_trace(go.Scatter(x=x, y=load, mode='none', name='demand', line={'width': 2, 'color': 'orange'},
-                  fill='tonexty'))
-    fig.update_layout(template='plotly_dark', title='Supply/Demand after Power Scaling',
-                      plot_bgcolor='#23272c', paper_bgcolor='#23272c', yaxis_title='MW',
-                      xaxis_title='Date/Time')
-    return fig
+# Update the index
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return main_page
+    if pathname == '/about':
+         return about_page
+    elif pathname == '/details':
+         return details_page(app)
+    else:
+        return '404'
 
 
 if __name__ == '__main__':
